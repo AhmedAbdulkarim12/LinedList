66e929adb8ab7a1498a24e7278b9ce7eead5f276
‪Ahmed Abdulkarim‬‏ <67345096+AhmedAbdulkarim12@users.noreply.github.com>
2024-06-13 20:18:40 +0300

    Add files via upload

---
+++ LinkedList.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LinkedList_Project
+{
+    public class LinkedList<T>
+    {
+        private Node<T> head;
+
+        public void AddFirst(T _data)
+        {
+            Node<T> newNode = new Node<T>();
+
+            newNode.data = _data;
+            newNode.next = head;
+
+            head = newNode;
+        }
+
+
+        public void AddLast(T _data)
+        {
+            if (head == null)
+            {
+                head = new Node<T>();
+
+                head.data = _data;
+                head.next = null;
+            }
+            else
+            {
+                Node<T> newNode = new Node<T>();
+                newNode.data = _data;
+
+                Node<T> current = head;
+                while (current.next != null)
+                {
+                    current = current.next;
+                }
+                current.next = newNode;
+            }
+        }
+
+        public void AddINIndex(int IndexNum, T _data)
+        {
+            Node<T> newNode = new Node<T>();
+            newNode.data = _data;
+
+            if (IndexNum == 0)
+            {
+                newNode.next = head;
+                head = newNode;
+                return;
+            }
+
+            int currentIndex = 1;
+            Node<T> prev = head;
+            Node<T> current = head.next;
+
+            while (current != null)
+            {
+                if (currentIndex == IndexNum)
+                {
+                    newNode.next = current;
+                    prev.next = newNode;
+                    current = newNode;
+                    return ;
+                }
+                prev = current;
+                current = current.next;
+
+                currentIndex++;
+            }
+
+        }
+
+        public void Reverse()
+        {
+            Node<T> prev = null;
+            Node<T> current = head;
+
+            if (current == null)
+                return;
+
+            while (current != null)
+            {
+                Node<T> next = current.next;
+                current.next = prev;
+                prev = current;
+                current = next;
+            }
+
+            head = prev;
+        }
+
+        public void Remove(int IndexNum)
+        {
+            if (head == null)
+                return ;
+
+            if (IndexNum == 0)
+            {
+                head = head.next;
+            }
+
+            int count = 0;
+            var current = head;
+            while (current != null)
+            {
+                count++;
+                if (count == IndexNum)
+                {
+                    current.next = current.next.next;
+                    return ;
+                }
+                current = current.next;
+            }
+
+        }
+
+        public void PrintAll()
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                Console.WriteLine(current.data );
+                current = current.next;
+            }
+            
+        }
+
+       public void PrintedReverse()
+        {
+            Reverse();
+            PrintAll();
+            Reverse();
+        }
+
+        public int Size()
+        {
+            int count = 0 ;
+            Node<T> current = head;
+            while (current != null)
+            {
+                current = current.next;
+                count++;
+            }
+            return count;
+        }
+
+        public Node<T> getNode(int IndexNum)
+        {
+            int currentIndex = 0;
+            Node<T> current = head;
+            while (current != null)
+            {
+                if(currentIndex == IndexNum)
+                {  return current; }
+                current = current.next;
+                currentIndex++;
+            }
+            return null;
+        }
+
+       
+        public int FindFirstIndex(T _data)
+        {
+            int currentIndex = 0;
+            Node<T> current = head;
+            while (current != null)
+            {
+                if (current.data.Equals(_data))
+                { return currentIndex; }
+                current = current.next;
+                currentIndex++;
+            }
+            return -1;
+        }
+
+
+        public bool IsNull()
+        {
+            return head == null ? true : false;
+        }
+
+        public void EmptyList()
+        {
+            head = null;
+        }
+
+    }
+
+
+
+    public class Node<T>
+    {
+        public T data;
+        public Node<T> next;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Console.WriteLine("Choose an option:");
+                Console.WriteLine("1. Create and display a Singly Linked List");
+                Console.WriteLine("2. Create a singly linked list of n nodes and display it in reverse order");
+                Console.WriteLine("3. Create a singly linked list of n nodes and count the number of nodes");
+                Console.WriteLine("4. Insert a node at any position in a Singly Linked List");
+                Console.WriteLine("5. Insert a node at the beginning of a Singly Linked List");
+                Console.WriteLine("6. Insert a node at the end of a Singly Linked List");
+                Console.WriteLine("7. Get a node in an existing singly linked list");
+                Console.WriteLine("8. Find the first index that matches a given element");
+                Console.WriteLine("9. Check whether a single linked list is empty or not");
+                Console.WriteLine("10. Empty a singly linked list");
+                Console.WriteLine("11. Remove the node from the singly linked list at the specified index");
+                Console.WriteLine("12. Calculate the size of a Singly Linked list");
+                Console.WriteLine("0. Exit");
+
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("");
+                        Q1();
+                        break;
+                    case 2:
+                        Console.WriteLine("");
+                        Q2();
+                        break;
+                    case 3:
+                        Console.WriteLine("");
+                        Q3();
+                        break;
+                    case 4:
+                        Console.WriteLine("");
+                        Q4();
+                        break;
+                    case 5:
+                        Console.WriteLine("");
+                        Q5();
+                        break;
+                    case 6:
+                        Console.WriteLine("");
+                        Q6();
+                        break;
+                    case 7:
+                        Console.WriteLine("");
+                        Q7();
+                        break;
+                    case 8:
+                        Console.WriteLine("");
+                        Q8();
+                        break;
+                    case 9:
+                        Console.WriteLine("");
+                        Q9();
+                        break;
+                    case 10:
+                        Console.WriteLine("");
+                        Q10();
+                        break;
+                    case 11:
+                        Console.WriteLine("");
+                        Q11();
+                        break;
+                    case 12:
+                        Console.WriteLine("");
+                        Q12();
+                        break;
+                    case 0:
+                        Console.WriteLine("Exiting...");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid option. Please choose again.");
+                        break;
+                }
+
+                Console.WriteLine(); // Add a blank line for better readability
+            }
+
+        }
+
+
+        //Write a  c# program to create and display a Singly Linked List.
+        static void Q1()
+        {
+            LinkedList<string> list = new LinkedList<string>();
+            list.AddLast("a");
+            list.AddLast("b");
+            list.AddLast("c");
+            list.AddLast("d");
+            list.AddLast("e");
+
+            list.PrintAll();
+        }
+
+        //Write a  c# program to create a singly linked list of n nodes and display it in reverse order.
+        static void Q2()
+        {
+            LinkedList<string> list = new LinkedList<string>();
+            list.AddLast("a");
+            list.AddLast("b");
+            list.AddLast("c");
+            list.AddLast("d");
+            list.AddLast("e");
+
+            Console.WriteLine("----- Print -----");
+            list.PrintAll();
+            Console.WriteLine("----- Print Reverse order -----");
+            list.PrintedReverse();
+        }
+
+        //Write a  c# program to create a singly linked list of n nodes and count the number of nodes.
+        static void Q3()
+        {
+            Console.WriteLine("Enter the number of nodes you want to create:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n == 0) { return; }
+
+            LinkedList<string> list = new LinkedList<string>();
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Enter data for nodes number " + i +" :");
+                list.AddLast(Console.ReadLine());
+            }
+
+            Console.WriteLine("You have entered the value of all nodes");
+            Console.WriteLine("----- Print -----");
+            list.PrintAll();
+        }
+
+        //Write a  c# program to insert a node at any position in a Singly Linked List.
+        static void Q4()
+        {
+            LinkedList<string> list = new LinkedList<string>();
+            list.AddLast("a");
+            list.AddLast("b");
+            list.AddLast("c");
+            list.AddLast("d");
+            list.AddLast("e");
+
+            Console.WriteLine("----- Print -----");
+            list.PrintAll();
+
+            Console.WriteLine("----- Print after add h in position 2 -----");
+            list.AddINIndex(2, "g");
+            list.PrintAll();
+        }
+
+        // Write a  c# program to insert a node at the beginning of a Singly Linked List.
+        static void Q5()
+        {
+            LinkedList<string> list = new LinkedList<string>();
+            list.AddLast("a");
+            list.AddLast("b");
+            list.AddLast("c");
+            list.AddLast("d");
+            list.AddLast("e");
+
+            Console.WriteLine("----- Print -----");
+            list.PrintAll();
+
+            Console.WriteLine("----- Print after add h in the beginning -----");
+            list.AddFirst("h");
+            list.PrintAll();
+        }
+
+
+        //Write a  c# program to insert a node at the end of a Singly Linked List.
+        static void Q6()
+        {
+            LinkedList<string> list = new LinkedList<string>();
+            list.AddLast("a");
+            list.AddLast("b");
+            list.AddLast("c");
+            list.AddLast("d");
+            list.AddLast("e");
+
+            Console.WriteLine("----- Print -----");
+            list.PrintAll();
+
+            Console.WriteLine("----- Print after add h in the end -----");
+            list.AddLast("h");
+            list.PrintAll();
+        }
+
+        //Write a  c# program to get a node in an existing singly linked list.
+        static void Q7()
+        {
+            LinkedList<string> list = new LinkedList<string>();
+            list.AddLast("a");
+            list.AddLast("b");
+            list.AddLast("c");
+            list.AddLast("d");
+            list.AddLast("e");
+
+            Console.WriteLine("----- Print -----");
+            list.PrintAll();
+
+            Console.WriteLine("----- GET AND Print Node num 3 -----");
+            var Node3 = list.getNode(3);
+            Console.WriteLine("Node number: 3");
+            Console.WriteLine("Node data:" + Node3.data);
+        }
+
+
+        //Write a  c# program to find the first index that matches a given element. Return -1 for no matching.
+        static void Q8()
+        {
+            LinkedList<string> list = new LinkedList<string>();
+            list.AddLast("a");
+            list.AddLast("b");
+            list.AddLast("c");
+            list.AddLast("d");
+            list.AddLast("e");
+
+            Console.WriteLine("----- Print -----");
+            list.PrintAll();
+
+            Console.WriteLine("----- find the first index that matches a given element (c) -----");
+            Console.WriteLine("Index: " + list.FindFirstIndex("c"));
+        }
+
+        //Write a  c# program to check whether a single linked list is empty or not. Return true otherwise false.
+        static void Q9()
+        {
+            LinkedList<string> list = new LinkedList<string>();
+            list.AddLast("a");
+            list.AddLast("b");
+            list.AddLast("c");
+            list.AddLast("d");
+            list.AddLast("e");
+
+            Console.WriteLine("----- Print -----");
+            list.PrintAll();
+
+            Console.WriteLine("-----single linked list is empty : " + list.IsNull());
+            
+        }
+
+
+        //Write a  c# program to empty a singly linked list by pointing the head towards null.
+        static void Q10()
+        {
+            LinkedList<string> list = new LinkedList<string>();
+            list.AddLast("a");
+            list.AddLast("b");
+            list.AddLast("c");
+            list.AddLast("d");
+            list.AddLast("e");
+
+            Console.WriteLine("----- Print -----");
+            list.PrintAll();
+
+            list.EmptyList();
+
+            Console.WriteLine("----- Print after empty a singly linked list -----");
+            list.PrintAll();
+        }
+
+        //Write a  c# program that removes the node from the singly linked list at the specified index.
+        static void Q11()
+        {
+            LinkedList<string> list = new LinkedList<string>();
+            list.AddLast("a");
+            list.AddLast("b");
+            list.AddLast("c");
+            list.AddLast("d");
+            list.AddLast("e");
+
+            Console.WriteLine("----- Print -----");
+            list.PrintAll();
+
+            list.Remove(2);
+
+            Console.WriteLine("----- Print after remove node in index 2 -----");
+            list.PrintAll();
+        }
+
+        // 12. Write a  c# program that calculates the size of a Singly Linked list.
+        static void Q12()
+        {
+            LinkedList<string> list = new LinkedList<string>();
+            list.AddLast("a");
+            list.AddLast("b");
+            list.AddLast("c");
+            list.AddLast("d");
+            list.AddLast("e");
+
+            Console.WriteLine("----- Print -----");
+            list.PrintAll();
+            Console.WriteLine("list Size: " + list.Size());
+
+        }
+
+    }
+
+}
